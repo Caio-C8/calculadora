1f40ccb405bcabf02f2ef2f1227af2f365d48cbb
Caio-C8 <caiomgf50@gmail.com>
2025-08-02 12:00:50 -0300

    feat: permite o frontend consumir a api

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.history_routes import history_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(history_router, prefix="/history")
 
 
